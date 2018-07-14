430e753ab5050770ea27f5b16bd879e2cc99712b
sjlutterbie <simon@ltrbe.com>
2018-07-14 17:47:01 +0000

    Moved early experiments to simpleSite folder; preparing for Express tutorial

---
+++ simpleSite/credentials.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+//Sandbox MySQL
+
+exports.mysqlcreds = {
+    host: 'sjlutterbie-sandbox.chpkk719ls8o.us-west-2.rds.amazonaws.com',
+    user: 'sjlutterbie',
+    password: 'sandboxsjlutterbie'
+};
