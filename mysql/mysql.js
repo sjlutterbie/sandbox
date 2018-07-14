42c0c40f828cf610b5983bff0ee22596b734b041
sjlutterbie <simon@ltrbe.com>
2018-07-14 17:19:29 +0000

    Established basic MySQL connectivity and credentials file

---
+++ mysql/mysql.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+var creds = require('../credentials');
+
+var con = mysql.createConnection({
+    host: creds.mysqlcreds['host'],
+    user: creds.mysqlcreds['user'],
+    password: creds.mysqlcreds['password'],
+    database: 'sandbox'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  
+  con.query('SELECT * FROM birthdays;', function (err, result, fields) {
+    if (err) throw err;
+    
+    for (var i in result) {
+      for (var j in result[i]){
+        var tmpString = j + ': ' + result[i][j];
+        console.log(tmpString);
+      }
+    }
+  });
+});
